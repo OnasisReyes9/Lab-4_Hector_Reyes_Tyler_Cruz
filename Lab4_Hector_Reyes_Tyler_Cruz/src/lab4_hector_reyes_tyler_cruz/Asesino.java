f47b90231e73e799dfe10ebf42acfee783d40c24
KysTyler <62519436+KysTyler@users.noreply.github.com>
2020-08-14 16:37:34 -0600

    Added cometer delito method

--- Lab4_Hector_Reyes_Tyler_Cruz/src/lab4_hector_reyes_tyler_cruz/Asesino.java
+++ Lab4_Hector_Reyes_Tyler_Cruz/src/lab4_hector_reyes_tyler_cruz/Asesino.java
@@ -12,6 +12,48 @@ package lab4_hector_reyes_tyler_cruz;
 public class Asesino {
     
     private String nombreVictima, arma;
+
+    public Asesino() {
+    }
+
+    public Asesino(String nombreVictima, String arma) {
+        this.nombreVictima = nombreVictima;
+        this.arma = arma;
+    }
+
+    public String getNombreVictima() {
+        return nombreVictima;
+    }
+
+    public void setNombreVictima(String nombreVictima) {
+        this.nombreVictima = nombreVictima;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+    
+    void CometerDelito(String nombre,String arma,Criminal c){
+        try {
+        validacion(c.getDelitos().size(),nombre,arma);
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        
+    }
+    void validacion(int x,String name,String weapon) throws MyException{
+        if (x > 1) {
+            throw new MyException ("El ataque no se pudo hacer");
+        }else{
+            throw new MyException("Nombre de la victima " + name + "en la ciudad" + weapon);
+        }
+        
+    }
     
     
 }
